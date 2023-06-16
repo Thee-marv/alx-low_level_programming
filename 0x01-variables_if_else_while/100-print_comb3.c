ff8b07c87d29277656ade5801e2417050cb5d4ec
Thee-marv <marvinanayo@gmail.com>
2023-06-16 14:16:56 -0700

    WARNING: Missing a blank line after declarations fixed for all

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 /**
- * main - Main function prints all possible different combinations of two digits
+ * main - prints all possible different combinations of two digits
  * Return: ALways 0 (Success)
  */
 int main(void)
 {
 	int n, m;
+	
 	for (n = 48; n <= 56; n++)
 	{
 		for (m = 49; m <= 57; m++)
