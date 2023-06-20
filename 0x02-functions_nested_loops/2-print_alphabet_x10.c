0a510e1ee4b97b0495f4fed950392ad8925e813f
Thee-marv <marvinanayo@gmail.com>
2023-06-20 10:44:46 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**void print_alphabet_x10(void prints letters 10X
+ * Using thre ASCII table
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int i, a;
+
+	for (i=1; i<=10; i++)
+	{
+		for (a=97; a<=122; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+}
